80d8af95566318133edd024e710df341d3c34d54
Satyam Prakash <euroyalsp@gmail.com>
2024-06-25 13:10:16 +0530

    Updated Responsive Js

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-document.querySelector('.site-mobile-menu').addEventListener('click', function () {
-    const header = document.getElementById('site-header');
-    const menu = document.querySelector('.navbar__menu');
-    const branding = document.getElementById('site-branding');
-    this.classList.toggle('active');
-    menu.classList.toggle('active');
-    branding.classList.toggle('hidden');
-    header.classList.toggle('mobile-menu');
+let mobileMenu = document.querySelector(".site-mobile-menu");
+let header = document.getElementById("site-header");
+let menu = document.querySelector(".navbar__menu");
+let branding = document.getElementById("site-branding");
+
+mobileMenu.addEventListener("click", function () {
+  const header = document.getElementById("site-header");
+  const menu = document.querySelector(".navbar__menu");
+  const branding = document.getElementById("site-branding");
+  this.classList.toggle("active");
+  menu.classList.toggle("active");
+  branding.classList.toggle("hidden");
+  header.classList.toggle("mobile-menu");
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 916) {
+      mobileMenu.classList.remove("active");
+      menu.classList.remove("active");
+      branding.classList.remove("hidden");
+      header.classList.remove("mobile-menu");
+    }
+  });
 });
 
-let liElements = document.querySelectorAll('li.navbar__item.navbar__dropdown');
-liElements.forEach(function(li) {
-    li.addEventListener('click', function() {
-        this.classList.toggle('active');
-        let dropdownMenu = this.querySelector('.navbar__dropdown-menu');
-        if (dropdownMenu) {
-            dropdownMenu.classList.toggle('show');
-        }
-    });
+let liElements = document.querySelectorAll("li.navbar__item.navbar__dropdown");
+liElements.forEach(function (li) {
+  li.addEventListener("click", function () {
+    this.classList.toggle("active");
+    let dropdownMenu = this.querySelector(".navbar__dropdown-menu");
+    if (dropdownMenu) {
+      dropdownMenu.classList.toggle("show");
+    }
+  });
 });
